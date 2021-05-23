7376615a8237042ceb7cfea7e562729a38e31da7
Yue Lu <luyuenudt@163.com>
2021-05-24 00:43:43 +0800

    Add project files.

---
+++ 1-two-sum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1_two_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 3,3 };
+            int target = 6;
+            Solution mySolution = new Solution();
+            int[] result = mySolution.TwoSum(nums, target);
+            foreach(var n in result)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
---
+++ 1-two-sum/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Source : https://leetcode.com/problems/two-sum/
+// Author : Yue Lu
+// Date   : 2021-05-23
+
+namespace _1_two_sum
+{
+    class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] ret = new int[2];
+            Dictionary<int, int> arrayDict = new Dictionary<int, int>();
+            for (int id = 0; id < nums.Length; id++)
+            {
+                // dynamic judge
+                if (arrayDict.ContainsKey(target - nums[id]))
+                {
+                    BuildResult(arrayDict[target - nums[id]], id, ret);
+                    return ret;
+                }
+                if (!arrayDict.ContainsKey(nums[id]))
+                {
+                    arrayDict.Add(nums[id], id);
+                }
+            }
+            return ret;
+        }
+
+        private void BuildResult(int index1, int index2, int[] ret)
+        {
+            ret[0] = index1;
+            ret[1] = index2;
+        }
+    }
+}
