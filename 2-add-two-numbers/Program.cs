cacd0e4ac2364932949f597a75d34100f7beb2c6
Yue Lu <luyuenudt@163.com>
2021-05-25 01:16:38 +0800

    problems 2

---
+++ 2-add-two-numbers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _2_add_two_numbers
+{
+    class Program
+    {
+        static void PrintListFromInputNode(ListNode node)
+        {
+            ListNode currentNode = node;
+            while (currentNode != null)
+            {
+                Console.WriteLine(currentNode.val);
+                currentNode = currentNode.next;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[] { 8 };
+            int[] array2 = new int[] { 9};
+            LinkList list1 = new LinkList();
+            LinkList list2 = new LinkList();
+
+            foreach (var n in array1)
+            {
+                list1.AddNode(n);
+            }
+            Console.WriteLine("List 1:");
+            PrintListFromInputNode(list1.GetFristNode());
+
+            foreach (var n in array2)
+            {
+                list2.AddNode(n);
+            }
+            Console.WriteLine("List 2:");
+            PrintListFromInputNode(list2.GetFristNode());
+
+            Solution sol = new Solution();
+            ListNode node = sol.AddTwoNumbers(list1.GetFristNode(), list2.GetFristNode());
+            Console.WriteLine("Result:");
+            PrintListFromInputNode(node);
+        }
+    }
+}
---
+++ 2-add-two-numbers/Solution.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Source : https://leetcode.com/problems/add-two-numbers/
+// Author : Yue Lu
+// Date   : 2021-05-25
+
+namespace _2_add_two_numbers
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val=0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class LinkList
+    {
+        private ListNode mHeadNode;
+
+        public LinkList()
+        {
+            mHeadNode = new ListNode();
+        }
+
+        public void AddNode(int val)
+        {
+            ListNode currentNode = mHeadNode;
+            ListNode newNode = new ListNode(val);
+            while (currentNode.next != null)
+            {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
+        }
+
+
+
+        public ListNode GetFristNode()
+        {
+            return mHeadNode.next;
+        }
+    }
+
+    class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode result = new ListNode();
+            ListNode nodeCruise = result;
+            int carrier = 0;
+            do
+            {
+                int currentVal = l1.val + l2.val + carrier;
+                carrier = currentVal / 10;
+                int units = currentVal % 10;
+                ListNode currentNode = new ListNode(units);
+                nodeCruise.next = currentNode;
+                nodeCruise = nodeCruise.next;
+                l1 = l1.next;
+                l2 = l2.next;
+            } while (l1!=null && l2!=null);
+
+            while (l1 != null)
+            {
+                int currentVal = l1.val + carrier;
+                carrier = currentVal / 10;
+                int units = currentVal % 10;
+                ListNode currentNode = new ListNode(units);
+                nodeCruise.next = currentNode;
+                nodeCruise = nodeCruise.next;
+                l1 = l1.next;
+            }
+
+            while (l2 != null)
+            {
+                int currentVal = l2.val + carrier;
+                carrier = currentVal / 10;
+                int units = currentVal % 10;
+                ListNode currentNode = new ListNode(units);
+                nodeCruise.next = currentNode;
+                nodeCruise = nodeCruise.next;
+                l2 = l2.next;
+            }
+
+            if(carrier != 0)
+            {
+                ListNode currentNode = new ListNode(carrier);
+                nodeCruise.next = currentNode;
+                nodeCruise = nodeCruise.next;
+            }
+            return result.next;
+        }
+    }
+}
