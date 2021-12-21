d62f66a3166048d4ea1b5cd42c6b5e13d8d508fc
YueLu0116 <bit_drinkwater@163.com>
2021-12-22 00:46:55 +0800

    Add clean version of power-of-two

--- Rust/power-of-two/src/lib.rs
+++ Rust/power-of-two/src/lib.rs
@@ -23,6 +23,9 @@ impl Solution {
         }
         false
     }
+    pub fn is_power_of_two_clean(n: i32) -> bool {
+        n > 0 && n.count_ones() == 1
+    }
 }
 
 #[cfg(test)]
@@ -30,26 +33,26 @@ mod tests {
     use super::Solution;
     #[test]
     fn sample1() {
-        assert_eq!(Solution::is_power_of_two(4), true);
+        assert_eq!(Solution::is_power_of_two_clean(4), true);
     }
     #[test]
     fn sample2() {
-        assert_eq!(Solution::is_power_of_two(1), true);
+        assert_eq!(Solution::is_power_of_two_clean(1), true);
     }
     #[test]
     fn sample3() {
-        assert_eq!(Solution::is_power_of_two(128), true);
+        assert_eq!(Solution::is_power_of_two_clean(128), true);
     }
     #[test]
     fn sample4() {
-        assert_eq!(Solution::is_power_of_two(-256), false);
+        assert_eq!(Solution::is_power_of_two_clean(-256), false);
     }
     #[test]
     fn sample5() {
-        assert_eq!(Solution::is_power_of_two(3), false);
+        assert_eq!(Solution::is_power_of_two_clean(3), false);
     }
     #[test]
     fn sample6() {
-        assert_eq!(Solution::is_power_of_two(0), false);
+        assert_eq!(Solution::is_power_of_two_clean(0), false);
     }
 }
